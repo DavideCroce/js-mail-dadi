ed317c2bc846ce242d50fa22ca1dbc48818667e4
IskahnX <davidecroce10@gmail.com>
2022-01-10 16:22:09 +0100

    start array emails

---
+++ es mail/js/script.js
@@ -0,0 +1,7 @@
+console.log (' JS OK ');
+
+const userMail = prompt ('Inserisci la tua mail');
+console.log (userMail);
+
+const email = ['davidecroce10@gmail.com', 'littleruruki45@gmail.com', 'kurokinokenshi@gmail.com'];
+console.table (email);
