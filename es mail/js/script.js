c580d745e60c0462487da7d80773848d8e20d36d
IskahnX <davidecroce10@gmail.com>
2022-01-10 16:40:53 +0100

    start cycle for emails

--- es mail/js/script.js
+++ es mail/js/script.js
@@ -5,3 +5,18 @@ console.log (userMail);
 
 const email = ['davidecroce10@gmail.com', 'littleruruki45@gmail.com', 'kurokinokenshi@gmail.com'];
 console.table (email);
+
+for (let i = 0; i < email.length; i++);{
+    if (userMail = [0]){
+        const message = ('Sei nella lista');
+    }
+    else if (userMail = [1]){
+        const message = ('Sei nella lista');
+    }
+    else if ( userMail = [2]){
+        const message = ('Sei nella lista');
+    }
+    else{
+        const message = ('Non sei nella lista');
+    }
+}
